467cc79ba5d454864ab015812599e85b6181fe84
F0X <f0xde@protonmail.com>
2021-01-12 12:36:14 +0100

    Convert to gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.ben-manes.versions") version "0.36.0"
+}
+
+group = "org.ddnss.xecortex"
+version = "0.0.1"
+
+repositories {
+    jcenter()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.dmulloy2.net/nexus/repository/public/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.16.4-R0.1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.5.0")
+}
+
+val javaVersion = JavaVersion.VERSION_1_8
+
+java {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+    }
+
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+}
